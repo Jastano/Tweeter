a7348338b0ec1dbc8e7934cbcb5943b7c52cc496
JasTano <jasmine.broodhagen@outlook.com>
2025-07-04 21:27:37 -0400

    Fetch tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -51,36 +51,7 @@ $(document).ready(function () {
       });
   };
 
-  // Initial test tweets
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac"
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants"
-      },
-      created_at: 1461116232227
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd"
-      },
-      content: {
-        text: "Je pense , donc je suis"
-      },
-      created_at: 1461113959088
-    }
-  ];
-
-  // Render test tweets
-  renderTweets(data);
-
-  // Initial load (if connected to server)
+  // Handles real tweets
   loadTweets();
 
   // Submit handler
